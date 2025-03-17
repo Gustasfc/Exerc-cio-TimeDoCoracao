64308730681cc9cbda510cdc8e546dd7eb2d1de9
Gustavo Gonzalez <gustavorgonzalezgja@gmal.com>
2025-03-17 19:32:25 -0300

    TimeCoracao

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.Write("BA");
+
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.Write("RC");
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.Write("LO");
+
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.Write("NA");
+
+
+
+Console.ResetColor();
